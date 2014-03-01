7a74f943efc01afb658295f31df33424445e1e76
RedOne <RedOne@RedOne-PC>
2014-03-01 23:53:18 +0000

    formation version 1.3

--- src/mqlUIT/CandidatureBundle/Form/CandidatureType1.php
+++ src/mqlUIT/CandidatureBundle/Form/CandidatureType1.php
@@ -9,27 +9,52 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class CandidatureType1 extends AbstractType
 {
     protected $user;
+    protected $type;
 
-public function __construct ($profile)
+public function __construct ($utilisateur,$typ)
 {
-    $this->user = $profile;
+    $this->user = $utilisateur;
+    $this->type = $typ;
 }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        
+        
       $user = $this->user;
-        $builder
-           // ->add('isvalid')
-            // ->add('candidat')
-            ->add('filiere', 'entity', array(
-        'class' => 'mqlUITCandidatureBundle:filiere',
-        'query_builder' => function(EntityRepository $er) use ($user) {
-         return $er->createQueryBuilder('f')
-               // ->Join('mqlUITCandidatureBundle:candidature', 'c', 'WITH', 'f.id = c.filiere')               
-                ->where('f.id NOT IN (select ff.id from mqlUITCandidatureBundle:candidature c join c.filiere ff where c.candidat = :id)')                
-                ->orderBy('f.intitule', 'ASC')
-                ->setParameter('id',$user);
-    },
+      if ($this->type === 1) {
+$builder
+->add('filiere', 'entity', array(
+'class' => 'mqlUITCandidatureBundle:filiere',
+'query_builder' => function(EntityRepository $er) use ($user) {
+return $er->createQueryBuilder('f')             
+->where('f.id NOT IN (select ff.id from mqlUITCandidatureBundle:candidature c join c.filiere ff where c.candidat = :id)')                
+->andWhere('f.grade = 1')
+->orderBy('f.intitule', 'DESC')
+->setParameter('id',$user);
+},
+));  
+
+
+      }else {
+          
+          
+$builder
+->add('filiere', 'entity', array(
+'class' => 'mqlUITCandidatureBundle:filiere',
+'query_builder' => function(EntityRepository $er) use ($user) {
+return $er->createQueryBuilder('f')        
+->where('f.id NOT IN (select ff.id from mqlUITCandidatureBundle:candidature c join c.filiere ff where c.candidat = :id)')                
+->andWhere('f.grade = 2')
+->orderBy('f.intitule', 'DESC')
+->setParameter('id',$user);
+},
 ));
+
+      }
+      
+
+    
+    
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
